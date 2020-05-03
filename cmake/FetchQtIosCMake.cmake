a9df31420caa1bb937f9d1aca5365a62f709d544
Olivier Ldff <olivier.ldff@gmail.com>
2020-05-03 15:25:39 +0200

    Customizable repository and tag url from cli

--- cmake/FetchQtIosCMake.cmake
+++ cmake/FetchQtIosCMake.cmake
@@ -1,9 +1,12 @@
 include(FetchContent)
 
+set(QQTIOSCMAKE_REPOSITORY "https://github.com/OlivierLDff/QtIosCMake.git" CACHE STRING "QtIosCMake repository, can be a local URL")
+set(QQTIOSCMAKE_TAG master CACHE STRING "QtIosCMake git tag")
+
 FetchContent_Declare(
   QtIosCMake
-  GIT_REPOSITORY https://github.com/OlivierLDff/QtIosCMake
-  GIT_TAG        master
+  GIT_REPOSITORY ${QQTIOSCMAKE_REPOSITORY}
+  GIT_TAG        ${QQTIOSCMAKE_TAG}
 )
 
 FetchContent_MakeAvailable(QtIosCMake)
